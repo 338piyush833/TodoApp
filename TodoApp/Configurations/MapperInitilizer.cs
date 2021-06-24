16d4f010ff8c78ed328334107f6f1448a65ef04c
338piyush833 <piyush.sharma@intimetec.com>
2021-06-24 22:07:26 +0530

    mapper and DTO created

---
+++ TodoApp/Configurations/MapperInitilizer.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoApp.Data;
+using TodoApp.Models;
+
+namespace TodoApp.Configurations
+{
+    public class MapperInitilizer : Profile
+    {
+        public MapperInitilizer()
+        {
+            CreateMap<TodoTask, TodoTaskDTO>().ReverseMap();
+            CreateMap<TodoTask, CreateTodoTaskDTO>().ReverseMap();
+        }
+    }
+}
---
+++ TodoApp/Models/TodoTaskDTO.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TodoApp.Models
+{
+    public class CreateTodoTaskDTO
+    {
+        [Required]
+        [StringLength(maximumLength:50,ErrorMessage = "Title Is Too Long")]
+        public string Title { get; set; }
+        [Required]
+        public string description { get; set; }
+        [Required]
+        [StringLength(maximumLength: 10, ErrorMessage = "status Is Too Long")]
+        public string status { get; set; }
+    }
+    public class TodoTaskDTO : CreateTodoTaskDTO
+    {
+        public int Id { get; set; }
+        
+    }
+}
